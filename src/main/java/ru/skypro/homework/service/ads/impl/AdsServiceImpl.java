56ce70f3caa20d2e928b3fc0bfd0b454fbd0da4c
Иван <ivanpaskhin@yandex.ru>
2023-09-07 14:11:13 +0300

    Добавлена (не полносью)  AdService и ссылка на файлы в application.properties

--- src/main/java/ru/skypro/homework/service/ads/impl/AdsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/ads/impl/AdsServiceImpl.java
@@ -1,40 +1,71 @@
 package ru.skypro.homework.service.ads.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.homework.dto.ads.AdDto;
-import ru.skypro.homework.dto.ads.AdsDto;
-import ru.skypro.homework.dto.ads.CreateOrUpdateAdDto;
-import ru.skypro.homework.dto.ads.ExtendedAdDto;
+import ru.skypro.homework.dto.ads.*;
+import ru.skypro.homework.entity.ads.Ad;
+import ru.skypro.homework.repository.ads.AdsRepository;
 import ru.skypro.homework.service.ads.AdsService;
+
+import java.io.IOException;
 
 @Service
 public class AdsServiceImpl implements AdsService {
 
+    @Autowired
+    private AdsRepository adsRepository;
+    @Autowired
+    private AdMapper adMapper;
+    @Value("${file.download.path}")
+    String imageFolderPath;
+
     @Override
     public AdsDto getAllAds() {
-        return null;
+        AdsDto result = adMapper.toAdsDto(adsRepository.findAll());
+        return result;
     }
 
     @Override
     public AdDto addAd(CreateOrUpdateAdDto createOrUpdateAdDto, MultipartFile image) {
-        return null;
+        //TODO сохраняет всё, кроме id Юзера
+        Ad newAd = adMapper.toEntity(createOrUpdateAdDto);
+        String imageName = image.getOriginalFilename();
+        String filePath = imageFolderPath;
+
+        newAd.setTitle(createOrUpdateAdDto.getTitle());
+        newAd.setPrice(createOrUpdateAdDto.getPrice());
+        newAd.setDescription(createOrUpdateAdDto.getDescription());
+
+        newAd.setImage(filePath + imageName);
+        adsRepository.save(newAd);
+        return adMapper.toAdDto(newAd);
     }
+
 
     @Override
     public ExtendedAdDto getAds(Integer id) {
-        return null;
+        //TODO сохраняет всё, кроме полей юзера
+        ExtendedAdDto extendedAdDto = adMapper.toExtendedAdDto(adsRepository.findById(id).get());
+        extendedAdDto.setTitle(adsRepository.findById(id).get().getTitle());
+        return extendedAdDto;
     }
+
 
     @Override
     public void removeAd(Integer id) {
-
+        //todo выдаёт 500 ошибку, если ad не найден в дб
+        adsRepository.deleteById(id);
     }
 
     @Override
     public AdDto updateAds(Integer id, CreateOrUpdateAdDto createOrUpdateAdDto) {
-
-        return null;
+        //todo выдаёт ошибку 415 при патче
+        Ad updatedAd = adMapper.toEntity(createOrUpdateAdDto);
+        updatedAd.setPk(id);
+        adsRepository.save(updatedAd);
+        return adMapper.toAdDto(updatedAd);
     }
 
     @Override
